c04ee440ce971255c5ec161bc8ae323392b9d2e1
work <youssef.ahmed85@yahoo.com>
2026-08-18 11:10:25 +0300

    refactor: enhance CORS handling by using Set for allowed origins and adding origin normalization

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -14,7 +14,7 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5500',
-  'https://todolist-naruto9.vercel.app/',
+  'https://todolist-naruto9.vercel.app',
   'https://todolist-xi-pied-46.vercel.app'
 ];
 
